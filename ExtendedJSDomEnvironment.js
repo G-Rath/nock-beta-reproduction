eb48c7942b5e6b2d2ae47b5d139b1e7c43f7e76e
Gareth Jones <jones258@gmail.com>
2024-09-27 08:25:30 +1200

    feat: improve things

--- ExtendedJSDomEnvironment.js
+++ ExtendedJSDomEnvironment.js
@@ -1,11 +1,11 @@
-import JSDOMEnvironment from 'jest-environment-jsdom';
+const { default: JSDOMEnvironment } = require('jest-environment-jsdom');
 
 /**
  * Provides an extended JSDOM-based environment that includes additional globals
  * that are not yet supported by JSDOM out of the box, using Node native equivalents
  */
-export default class ExtendedJSDOMEnvironment extends JSDOMEnvironment {
-  public constructor(...args: ConstructorParameters<typeof JSDOMEnvironment>) {
+class ExtendedJSDOMEnvironment extends JSDOMEnvironment {
+  constructor(...args) {
     super(...args);
 
     // see https://github.com/jsdom/jsdom/issues/1724
@@ -17,3 +17,5 @@ export default class ExtendedJSDOMEnvironment extends JSDOMEnvironment {
     this.global.TextEncoder = TextEncoder;
   }
 }
+
+module.exports = ExtendedJSDOMEnvironment;
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
-module.exports = {
-  //testEnvironment: 'jsdom',
-  //testEnvironment: './ExtendedJSDomEnvironment.ts',
-};
+const testEnvironment =
+  process.env.JEST_TEST_ENVIRONMENT === 'jsdom'
+    ? 'jsdom'
+    : process.env.JEST_TEST_ENVIRONMENT === 'extended'
+      ? './ExtendedJSDomEnvironment.js'
+      : null;
+
+console.log('using', testEnvironment, 'as the test environment');
+
+module.exports = { testEnvironment };
